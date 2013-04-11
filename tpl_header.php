50e8aa3a2e6acc7d7607c28547d10dc84c2155d3
BlockMen <nmuelll@web.de>
2013-04-11 17:26:23 +0300

    remove "account" from nav

--- tpl_header.php
+++ tpl_header.php
@@ -52,15 +52,15 @@ echo"
 <li>|</li>
 <li><a href='http://wiki.minetest.com/wiki/'>Wiki</a></li>
 <li style='float:right;'>
-                     <?php
+                    <?php
                         if ($_SERVER['REMOTE_USER']) {
 					 $USERINFO = $_SESSION[DOKU_COOKIE]['auth']['info'];
                             echo "<a href='' style='font-weight:bold;font-size:16px';>" . $USERINFO['name'] . "</a><ul><li><a href='". $nav_pre . $ID ."&do=recent' class='sub'>Changelog</a></li>";
-				   } else { echo "<a href='' style='font-size:18px';'>Account</a><ul>";}
+				  
                         tpl_action('admin', 1, 'li');
                         tpl_action('profile', 1, 'li');
                         tpl_action('register', 1, 'li');
-                        tpl_action('login', 1, 'li');
+                        tpl_action('login', 1, 'li'); }
                     ?>
                 </ul></li></ul>
             </div>
